d0c062223fa194e9685b4c369e2655ad5909c58a
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-04-27 22:27:02 +0700

    add validation for create & update data teachers

--- src/modules/admin/routes/teachersRouter.js
+++ src/modules/admin/routes/teachersRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const teachersController = require("../controllers/teachersController");
+const teachersValidator = require("../validator/teacher/teacher.validation");
 
 // Router Teachers
 router.get("/guru", teachersController.getAll); // get all teachers
 router.get("/guru/:guru_id", teachersController.getById); // get teacher by id
-router.post("/guru/tambah", teachersController.create); // create teacher
-router.put("/guru/ubah/:guru_id", teachersController.update); // update teacher
+router.post("/guru/tambah", teachersValidator.create, teachersController.create); // create teacher
+router.put("/guru/ubah/:guru_id", teachersValidator.create, teachersController.update); // update teacher
 router.delete("/guru/hapus/:guru_id", teachersController.delete); // delete teacher
 
 module.exports = router;
