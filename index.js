ecd91781acf75519802ac945c5daecda062b44f0
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-04-27 12:37:13 +0700

    Update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express(); // Create express app
 const port = 8000; // port localhost
 
 // use NPM
-app.use("/public", express.static(path.join(__dirname, "/public"))); // Static folder for images
+app.use("/public", express.static(path.join(__dirname, "/public"))); // Static folder for file public
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors()); // use cors for cross origin
@@ -20,13 +20,10 @@ app.use(function (req, res, next) {
     req.con = con;
     next();
 });
-
 // import admin router
-const adminAuth = require("./src/modules/admin/routes/authRouter");
+const adminAuth = require("./src/modules/admin/routes/authRouter"); // Router Login Admin
 
 // use admin router
-app.use(adminAuth);
+app.use(adminAuth); // use router login admin
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-});
+app.listen(port, () => { console.log(`Server is running on port ${port}`) }); // listen port
