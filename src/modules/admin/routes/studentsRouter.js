9f490bc2c508aef7c9c5418eb97424ac90b7299d
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-04-27 17:26:11 +0700

    edit endpoint get image students

--- src/modules/admin/routes/studentsRouter.js
+++ src/modules/admin/routes/studentsRouter.js
@@ -6,7 +6,7 @@ const studentValidator = require('../validator/student/student.validation')
 // router students
 router.get('/siswa', studentsController.getAll) // get all students
 router.get('/siswa/:siswa_id', studentsController.getById) // get student by id
-router.get('/foto/siswa/:kelas_id', studentsController.getByKelasId) // get student img by kelas id
+router.get('/siswa/gambar/:kelas_id', studentsController.getByKelasId) // get student img by kelas id
 router.post('/siswa/tambah', studentValidator.create, studentsController.create) // create student
 router.put('/siswa/ubah/:siswa_id', studentValidator.update, studentsController.update) // update student by id
 router.delete('/siswa/hapus/:siswa_id', studentsController.delete) // delete student by id
