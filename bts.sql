922c866fd463478009fdbd3d9a5492c37da3e985
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-04-27 16:36:45 +0700

    update database

--- bts.sql
+++ bts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2022 at 05:56 AM
+-- Generation Time: Apr 27, 2022 at 11:36 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.15
 
@@ -33,6 +33,13 @@ CREATE TABLE `admin` (
   `admin_email` varchar(100) NOT NULL,
   `admin_password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_nama`, `admin_email`, `admin_password`) VALUES
+(1, 'admin', 'admin@admin.com', 'admin');
 
 -- --------------------------------------------------------
 
@@ -108,17 +115,66 @@ INSERT INTO `jurusan` (`jurusan_id`, `jurusan_nama`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `kelas_id` int(11) NOT NULL,
+  `kelas_nama` varchar(50) NOT NULL,
+  `jurusan_id` int(4) NOT NULL,
+  `d_kelas_id` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`kelas_id`, `kelas_nama`, `jurusan_id`, `d_kelas_id`) VALUES
+(1, 'Akuntansi 1', 1, 1),
+(2, 'Akuntansi 2', 1, 2),
+(3, 'Akuntansi 3', 1, 3),
+(4, 'Rekayasa Perangkat Lunak 1', 2, 1),
+(5, 'Rekayasa Perangkat Lunak 2', 2, 2),
+(6, 'Rekayasa Perangkat Lunak 3', 2, 3),
+(7, 'Teknik Komputer dan Jaringan 1', 3, 1),
+(8, 'Teknik Komputer dan Jaringan 2', 3, 2),
+(9, 'Teknik Komputer dan Jaringan 3', 3, 3),
+(10, 'Teknik Elektronika Industri 1', 4, 1),
+(11, 'Teknik Elektronika Industri 2', 4, 2),
+(12, 'Teknik Elektronika Industri 3', 4, 3),
+(13, 'Teknik Elektronika Industri 4', 4, 4),
+(14, 'Teknik dan Bisnis Sepeda Motor 1', 5, 1),
+(15, 'Teknik dan Bisnis Sepeda Motor 2', 5, 2),
+(16, 'Teknik dan Bisnis Sepeda Motor 3', 5, 3),
+(17, 'Teknik dan Bisnis Sepeda Motor 4', 5, 4),
+(18, 'Teknik Energi Terbarukan 1', 6, 1),
+(19, 'Teknik Energi Terbarukan 2', 6, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
   `siswa_id` int(11) NOT NULL,
+  `siswa_nis` int(10) NOT NULL,
   `siswa_nama` varchar(225) NOT NULL,
   `siswa_gambar` varchar(255) NOT NULL,
   `siswa_quote` varchar(100) NOT NULL,
-  `jurusan_id` int(4) NOT NULL,
-  `d_kelas_id` int(4) NOT NULL
+  `kelas_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`siswa_id`, `siswa_nis`, `siswa_nama`, `siswa_gambar`, `siswa_quote`, `kelas_id`) VALUES
+(1, 192010001, 'Rafly', 'profile.png', 'ga bikin quote', 6),
+(2, 192010002, 'Raka', 'profile.png', 'ga bikin quote', 6),
+(3, 192010004, 'Abel', 'avatar1.png', 'ga bikin quote', 6),
+(5, 192010005, 'Ade', 'avatar1.png', 'ga bikin quote', 6),
+(7, 192010006, 'Asti', 'avatar2.png', 'ga bikin quote', 6);
 
 --
 -- Indexes for dumped tables
@@ -157,12 +213,20 @@ ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`jurusan_id`);
 
 --
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`kelas_id`),
+  ADD KEY `jurusan_id` (`jurusan_id`),
+  ADD KEY `d_kelas_id` (`d_kelas_id`);
+
+--
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`siswa_id`),
-  ADD KEY `jurusan_id` (`jurusan_id`),
-  ADD KEY `d_kelas_id` (`d_kelas_id`);
+  ADD UNIQUE KEY `siswa_nis` (`siswa_nis`),
+  ADD KEY `kelas_id` (`kelas_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -172,7 +236,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `d_kelas`
@@ -199,10 +263,16 @@ ALTER TABLE `jurusan`
   MODIFY `jurusan_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `kelas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `siswa_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `siswa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -216,11 +286,17 @@ ALTER TABLE `gambar`
   ADD CONSTRAINT `gambar_ibfk_2` FOREIGN KEY (`d_kelas_id`) REFERENCES `d_kelas` (`d_kelas_id`);
 
 --
+-- Constraints for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`jurusan_id`) REFERENCES `jurusan` (`jurusan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kelas_ibfk_2` FOREIGN KEY (`d_kelas_id`) REFERENCES `d_kelas` (`d_kelas_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `siswa`
 --
 ALTER TABLE `siswa`
-  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`jurusan_id`) REFERENCES `jurusan` (`jurusan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`d_kelas_id`) REFERENCES `d_kelas` (`d_kelas_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`kelas_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
