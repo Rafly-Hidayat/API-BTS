ba9c88cc316d643cc3b9f8b40439c68a64a85cb5
Rafly Hidayat <rafly.hidayat2710@gmail.com>
2022-06-30 18:58:32 +0700

    update validation add photo class

--- src/modules/admin/validator/photo/photo.schema.js
+++ src/modules/admin/validator/photo/photo.schema.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 module.exports = {
     // schema for create teacher
     create: joi.object({
-        jenis: joi.string().required(),
+        jenis: joi.string().valid('wajib','bebas').required(),
         kelas_id: joi.number().required(),
     })
 }
